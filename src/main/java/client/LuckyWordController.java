fa4608449c5ba2b4993d607edbe0a9564839314c
Moscoso Rivas <dlmo@gft.com>
2025-10-31 16:03:18 +0100

    Add support for Spring Cloud Bus with Kafka. Add Bruno collection

--- src/main/java/client/LuckyWordController.java
+++ src/main/java/client/LuckyWordController.java
@@ -1,6 +1,7 @@
 package client;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,16 +12,44 @@ import org.springframework.web.bind.annotation.RestController;
  * A default value is set just in case the server project is not started
  */
 @RestController
+@ConfigurationProperties(prefix="word-config")
 public class LuckyWordController {
 
     /**
-     * The default value is specified
+     * This is the config by default for lab-3
      */
-    @Value("${lucky-word:preparation}") String luckyWord;
+    /*@Value("${lucky-word:preparation}") String luckyWord;
 
     @GetMapping("/lucky-word")
     public String showLuckyWord() {
         return "The lucky word is: " + luckyWord;
+    }*/
+
+    /**
+     * This is the config for lab-5 (spring cloud bus)
+     */
+    String luckyWord;
+    String preamble;
+
+    @GetMapping("/lucky-word")
+    public String showLuckyWord() {
+        return preamble + ": " + luckyWord;
+    }
+
+    public String getLuckyWord() {
+        return luckyWord;
+    }
+
+    public void setLuckyWord(String luckyWord) {
+        this.luckyWord = luckyWord;
+    }
+
+    public String getPreamble() {
+        return preamble;
+    }
+
+    public void setPreamble(String preamble) {
+        this.preamble = preamble;
     }
 
 }
